164be1e6188dc3c72805379f3e31b36f5807f096
furuSchool <147578340+furuSchool@users.noreply.github.com>
2024-03-27 09:27:02 +0900

    不必要な変数などを削除

--- src/components/TwoDimensionalBarcode/StringToAnswer/index.tsx
+++ src/components/TwoDimensionalBarcode/StringToAnswer/index.tsx
@@ -6,11 +6,9 @@ interface SquareProps {
   x: number;
   y: number;
   fill: string;
-  toggleFill: () => void;
-  handleMouseEnter: () => void;
 }
 
-const Square = ({ x, y, fill, toggleFill, handleMouseEnter }: SquareProps) => (
+const Square = ({ x, y, fill }: SquareProps) => (
   <rect
     x={x}
     y={y}
@@ -19,8 +17,6 @@ const Square = ({ x, y, fill, toggleFill, handleMouseEnter }: SquareProps) => (
     fill={fill}
     stroke="lightgray"
     strokeWidth="0.05"
-    onMouseDown={toggleFill}
-    onMouseEnter={handleMouseEnter}
   />
 );
 
@@ -30,32 +26,6 @@ const CreateAnswer = () => {
   });
 
   const [isDragging, setIsDragging] = useState(false);
-
-  const toggleFill = (rowIndex: number, colIndex: number) => {
-    const newSquares = squares.map((row: string[], rIndex: number) =>
-      rIndex === rowIndex
-        ? row.map((col: string, cIndex: number) =>
-            cIndex === colIndex ? (col === "white" ? "black" : "white") : col,
-          )
-        : row,
-    );
-    setSquares(newSquares);
-  };
-
-  const handleMouseDown = (rowIndex: number, colIndex: number) => {
-    setIsDragging(true);
-    toggleFill(rowIndex, colIndex);
-  };
-
-  const handleMouseEnter = (rowIndex: number, colIndex: number) => {
-    if (isDragging) {
-      toggleFill(rowIndex, colIndex);
-    }
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
 
   const handleReset = () => {
     if (window.confirm("リセットします。よろしいですか？")) {
@@ -96,11 +66,8 @@ const CreateAnswer = () => {
             width="422"
             height="422"
             style={{
-              // border: "1px solid black",
-              // background: "lightgray",
               margin: "50px",
             }}
-            onMouseUp={handleMouseUp}
           >
             {squares.map((row: string[], rowIndex: number) =>
               row.map((_: string, colIndex: number) => (
@@ -109,8 +76,6 @@ const CreateAnswer = () => {
                   x={colIndex * 20 + 1}
                   y={rowIndex * 20 + 1}
                   fill={squares[rowIndex][colIndex]}
-                  toggleFill={() => handleMouseDown(rowIndex, colIndex)}
-                  handleMouseEnter={() => handleMouseEnter(rowIndex, colIndex)}
                 />
               )),
             )}
