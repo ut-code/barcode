c2c5e4a7a5052e37eeb2b57e9408622b88eda9b4
naka-12 <104970808+naka-12@users.noreply.github.com>
2024-04-10 16:59:01 +0900

    refactor: 機能・形式・データ・マスクのパートを関数に分離

--- src/components/TwoDimensionalBarcode/calculation/strTo2dBarcode.ts
+++ src/components/TwoDimensionalBarcode/calculation/strTo2dBarcode.ts
@@ -16,6 +16,122 @@ function encodeStrByMode(inputText: string, mode: EncodingMode) {
   }
 }
 
+function insertFunctionalPattern(cells: boolean[][]) {
+  // フォーマット情報を埋める
+  for (let i = 0; i < 21; ++i) {
+    for (let j = 0; j < 21; ++j) {
+      //   二次元コードの初期状態
+      if (i === 0 || i === 6) {
+        if ((j >= 0 && j < 7) || (j >= 14 && j < 21)) {
+          cells[i][j] = true;
+        }
+      } else if (j === 0 || j === 6) {
+        if ((i >= 0 && i < 7) || (i >= 14 && i < 21)) {
+          cells[i][j] = true;
+        }
+      }
+
+      if (i === 14 || i === 20) {
+        if (j >= 0 && j < 7) {
+          cells[i][j] = true;
+        }
+      } else if (j === 14 || j === 20) {
+        if (i >= 0 && i < 7) {
+          cells[i][j] = true;
+        }
+      }
+
+      if (
+        (i >= 2 && i < 5 && j >= 2 && j < 5) ||
+        (i >= 2 && i < 5 && j >= 16 && j < 19) ||
+        (i >= 16 && i < 19 && j >= 2 && j < 5)
+      ) {
+        cells[i][j] = true;
+      }
+
+      if ((i === 6 && j % 2 === 0) || (j === 6 && i % 2 === 0)) {
+        cells[i][j] = true;
+      }
+
+      cells[13][8] = true;
+    }
+  }
+  return cells;
+}
+
+function insertFormat(cells: boolean[][]) {
+  // 形式情報
+  for (let i = 0; i < 21; ++i) {
+    for (let j = 0; j < 21; ++j) {
+      const formatInformation: number = parseInt("101010000010010", 2);
+      if (j === 8 && i < 8) {
+        if (i < 6) {
+          if ((formatInformation >> i) % 2 === 1) {
+            cells[i][j] = true;
+          }
+        } else if (i > 6) {
+          if ((formatInformation >> (i - 1)) % 2 === 1) {
+            cells[i][j] = true;
+          }
+        }
+      } else if (j === 8 && i > 13) {
+        if ((formatInformation >> (i - 6)) % 2 === 1) {
+          cells[i][j] = true;
+        }
+      } else if (i === 8 && j < 8) {
+        if (j < 6) {
+          if ((formatInformation >> (14 - j)) % 2 === 1) {
+            cells[i][j] = true;
+          }
+        } else if (j > 6) {
+          if ((formatInformation >> (15 - j)) % 2 === 1) {
+            cells[i][j] = true;
+          }
+        }
+      } else if (i === 8 && j > 13) {
+        if ((formatInformation >> (20 - j)) % 2 === 1) {
+          cells[i][j] = true;
+        }
+      }
+    }
+  }
+  return cells;
+}
+
+function insertEncodedData(
+  cells: boolean[][],
+  bitData: bigint,
+  errorCorrectionCode: bigint,
+) {
+  // データを埋めていく
+  // データを埋める順番を示す配列を作成する。
+  const orderArrayForData: number[][] = makeOrderArrayForData();
+
+  // データを埋めていく
+  const totalData = (bitData << BigInt(10 * 8)) + errorCorrectionCode;
+  for (let i = 0; i < 26 * 8; ++i) {
+    if ((totalData >> BigInt(26 * 8 - i - 1)) % BigInt(2) === BigInt(1)) {
+      cells[orderArrayForData[i][0]][orderArrayForData[i][1]] = true;
+    }
+  }
+  return { cellsWithData: cells, orderArrayForData: orderArrayForData };
+}
+
+function mask(cellsWithFullData: boolean[][], orderArrayForData: number[][]) {
+  const cells = cellsWithFullData;
+  // マスク処理
+  for (let i = 0; i < 26 * 8; ++i) {
+    if ((orderArrayForData[i][0] + orderArrayForData[i][1]) % 2 === 0) {
+      if (cells[orderArrayForData[i][0]][orderArrayForData[i][1]] === true) {
+        cells[orderArrayForData[i][0]][orderArrayForData[i][1]] = false;
+      } else {
+        cells[orderArrayForData[i][0]][orderArrayForData[i][1]] = true;
+      }
+    }
+  }
+  return cells;
+}
+
 export default function strTo2dBarcode(
   inputText: string,
   mode: EncodingMode,
@@ -26,97 +142,16 @@ export default function strTo2dBarcode(
     .fill(false)
     .map(() => new Array(21).fill(false));
 
-  // フォーマット情報を埋める
-  for (let i = 0; i < 21; ++i) {
-    for (let j = 0; j < 21; ++j) {
-      //   二次元コードの初期状態
-      if (i === 0 || i === 6) {
-        if ((j >= 0 && j < 7) || (j >= 14 && j < 21)) {
-          newCells[i][j] = true;
-        }
-      } else if (j === 0 || j === 6) {
-        if ((i >= 0 && i < 7) || (i >= 14 && i < 21)) {
-          newCells[i][j] = true;
-        }
-      }
-
-      if (i === 14 || i === 20) {
-        if (j >= 0 && j < 7) {
-          newCells[i][j] = true;
-        }
-      } else if (j === 14 || j === 20) {
-        if (i >= 0 && i < 7) {
-          newCells[i][j] = true;
-        }
-      }
-
-      if (
-        (i >= 2 && i < 5 && j >= 2 && j < 5) ||
-        (i >= 2 && i < 5 && j >= 16 && j < 19) ||
-        (i >= 16 && i < 19 && j >= 2 && j < 5)
-      ) {
-        newCells[i][j] = true;
-      }
-
-      if ((i === 6 && j % 2 === 0) || (j === 6 && i % 2 === 0)) {
-        newCells[i][j] = true;
-      }
-
-      newCells[13][8] = true;
-      //   形式情報
-      const formatInformation: number = parseInt("101010000010010", 2);
-      if (j === 8 && i < 8) {
-        if (i < 6) {
-          if ((formatInformation >> i) % 2 === 1) {
-            newCells[i][j] = true;
-          }
-        } else if (i > 6) {
-          if ((formatInformation >> (i - 1)) % 2 === 1) {
-            newCells[i][j] = true;
-          }
-        }
-      } else if (j === 8 && i > 13) {
-        if ((formatInformation >> (i - 6)) % 2 === 1) {
-          newCells[i][j] = true;
-        }
-      } else if (i === 8 && j < 8) {
-        if (j < 6) {
-          if ((formatInformation >> (14 - j)) % 2 === 1) {
-            newCells[i][j] = true;
-          }
-        } else if (j > 6) {
-          if ((formatInformation >> (15 - j)) % 2 === 1) {
-            newCells[i][j] = true;
-          }
-        }
-      } else if (i === 8 && j > 13) {
-        if ((formatInformation >> (20 - j)) % 2 === 1) {
-          newCells[i][j] = true;
-        }
-      }
-    }
-  }
-
-  // データを埋めていく
-  // データを埋める順番を示す配列を作成する。
-  const orderArrayForData: number[][] = makeOrderArrayForData();
-
-  // データを埋めていく
-  const totalData = (bitData << BigInt(10 * 8)) + errorCorrectionCode;
-  for (let i = 0; i < 26 * 8; ++i) {
-    if ((totalData >> BigInt(26 * 8 - i - 1)) % BigInt(2) === BigInt(1)) {
-      newCells[orderArrayForData[i][0]][orderArrayForData[i][1]] = true;
-    }
-  }
-  // マスク処理
-  for (let i = 0; i < 26 * 8; ++i) {
-    if ((orderArrayForData[i][0] + orderArrayForData[i][1]) % 2 === 0) {
-      if (newCells[orderArrayForData[i][0]][orderArrayForData[i][1]] === true) {
-        newCells[orderArrayForData[i][0]][orderArrayForData[i][1]] = false;
-      } else {
-        newCells[orderArrayForData[i][0]][orderArrayForData[i][1]] = true;
-      }
-    }
-  }
-  return newCells;
+  const cellsWithFunctional = insertFunctionalPattern(newCells);
+  const cellsWithFunctionalAndFormat = insertFormat(cellsWithFunctional);
+  const {
+    cellsWithData: cellsWithFullData,
+    orderArrayForData: orderArrayForData,
+  } = insertEncodedData(
+    cellsWithFunctionalAndFormat,
+    bitData,
+    errorCorrectionCode,
+  );
+  const maskedCells = mask(cellsWithFullData, orderArrayForData);
+  return maskedCells;
 }
