8152aebafe7288f8327fff566162a8d43d00e3eb
furuSchool <147578340+furuSchool@users.noreply.github.com>
2024-05-12 14:45:48 +0900

    バリデーションの追加

--- src/components/TwoDimBarcode/features/CompletePlayground/index.tsx
+++ src/components/TwoDimBarcode/features/CompletePlayground/index.tsx
@@ -9,6 +9,7 @@ export default function CreateTwoDimBarcodePlayground() {
   const [cells, setCells] = useState<boolean[][]>(createNewCells());
   const [messageInput, setMessageInput] = useState<string>("");
   const [modeSelect, setModeSelect] = useState<EncodingMode>("eisu");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   useEffect(() => {
     // docusaurus の SSR への対応
@@ -29,7 +30,23 @@ export default function CreateTwoDimBarcodePlayground() {
         type="text"
         value={messageInput}
         onChange={(e) => {
-          setMessageInput(e.target.value);
+          const input = e.target.value;
+          setErrorMessage("");
+          if (modeSelect == "eisu") {
+            if (/^[A-Z0-9$%*+-./: ]*$/.test(input)) {
+              setMessageInput(input);
+            } else {
+              setErrorMessage("規格に当てはまらない文字です。");
+            }
+          } else if (modeSelect == "8bit") {
+            if (/^[\x00-\x7F]*$/.test(input)) {
+              setMessageInput(input);
+            } else {
+              setErrorMessage("規格に当てはまらない文字です。");
+            }
+          } else if (modeSelect == "sjis") {
+            setMessageInput(input);
+          }
         }}
       />
       <select onChange={(e) => setModeSelect(e.target.value as EncodingMode)}>
@@ -44,6 +61,7 @@ export default function CreateTwoDimBarcodePlayground() {
       >
         二次元コードを作成
       </button>
+      <p style={{ color: "red" }}>{errorMessage}</p>
       <TwoDimBarcode cells={cells} setCells={setCells} />
     </Playground>
   );
